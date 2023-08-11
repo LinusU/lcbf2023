2d780773ed937f743f0d37882923cbbfde246635
Linus Unnebäck <linus@folkdatorn.se>
2023-08-11 19:38:47 +0100

    Add location information

--- src/components/Beer.tsx
+++ src/components/Beer.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import { BsPinMapFill } from 'react-icons/bs'
 import { FaUntappd, FaCheck } from 'react-icons/fa'
 
 import { Beer as BeerType } from '../data.ts'
+import { theme } from '../lib/theme.ts'
 import { useCheckedIn, useSavedForLater } from '../lib/storage.ts'
 
 import IndexIndicator from './IndexIndicator.tsx'
 import Stars from './Stars.tsx'
 import Tag from './Tag.tsx'
 import Button from './Button.tsx'
+import { getLocation } from '../lib/location.ts'
 
 interface BeerProps {
     beer: BeerType
@@ -94,6 +97,15 @@ const Beer: React.FC<BeerProps> = ({ beer }) => {
                     <p>
                         {beer.brewery}
                     </p>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        margin: '6px 0'
+                    }}>
+                        <BsPinMapFill color={theme.primary}></BsPinMapFill>
+                        <p style={{ marginLeft: 10 }}>{getLocation(beer.brewery)}</p>
+                    </div>
                     <p>
                         ABV: {beer.abv.toFixed(2)}%
                     </p>
---
+++ src/lib/location.ts
@@ -0,0 +1,251 @@
+export function getLocation(brewery: string): string {
+  switch (brewery) {
+    // Vaults level
+
+    case 'Two Flints':
+    case 'Sureshot  Brewing Company':
+    case 'Holy Goat':
+    case 'Holy Goat Brewing':
+    case 'Little Monster':
+    case 'We are Beer Club':
+    case 'DEYA':
+    case 'Duration Brewing':
+    case 'Thornbridge Brewery':
+    case 'Pressure Drop':
+    case 'Northern Monk':
+    case 'Burning Sky':
+    case 'Track Brewery':
+      return 'NV1'
+
+    case 'Budweiser Budvar':
+      return 'NV2'
+
+    case 'Chouffe Brewery':
+      return 'NV3'
+
+    case 'Brew By Numbers':
+    case 'Kyro Distillery':
+      return 'NV4'
+
+    case 'RedLeg':
+      return 'NV5'
+
+    case 'Brixton Brewery':
+    case 'The Gipsy Hill Brewing':
+    case 'Indian Brewery':
+    case 'Timothy Taylor\'s':
+    case 'Haacht Brewery':
+    case 'Compass Box':
+      return 'NV6'
+
+    case 'O\'Hara\'s Brewing':
+    case 'ABK':
+    case 'Dartsee':
+      return 'SV1'
+
+    case 'Forest Road':
+      return 'SV2'
+
+    case 'Favela Cerveja':
+      return 'SV5'
+
+    case 'SALT Beer Factory':
+      return 'SV6'
+
+    case 'Amundsen Brewery':
+    case 'Amundsen Pastry World':
+      return 'SV7'
+
+    case 'Pretty Decent Beer Co':
+    case 'Hackney Church Brewery':
+    case 'The Wine Importers':
+    case 'The Mad Butcher':
+    case 'Jiddler\'s Tipple':
+    case 'Green Duck':
+    case 'Three Locks Brewing Co':
+    case 'Mammoth Beer':
+    case 'Siren Craft Brew':
+    case 'Round Corner Brewing':
+    case 'UnBarred Brewery':
+    case 'Mash Gang':
+    case 'Beyond Belief Brewing':
+    case '360 Degree Brewing':
+    case 'The Three Legs Brewing Co.':
+    case 'The Three Legs Brewing Co. ':
+      return 'Porter\'s Walk North'
+
+    case 'The Goodness Brew Co':
+    case 'Mikkeller':
+    case 'MIkkeller Brewpub London':
+    case 'Fierce Beer':
+    case 'Pimentae':
+    case 'North Brewing Co.':
+    case 'Utopian Brewing':
+    case 'New Bristol Brewery':
+    case 'Dugges':
+    case 'Frontaal Brewery':
+    case 'Galway Bay':
+    case 'Basqueland Brewing':
+      return 'North Bandstand'
+
+    case 'Yonder Brewing':
+    case 'The Five Points Brewing Company':
+    case 'Pillars Brewery':
+    case 'The Newt in Somerset':
+    case '40FT':
+      return 'Porter\'s Walk South'
+
+    case 'Real Handful':
+    case 'Jack Links':
+    case 'Botivo':
+    case 'Serious Pig':
+      return 'Central Staircase'
+
+    case 'Verdant Brewing Co.':
+    case 'Phantom Brewing Co.':
+    case 'Beak Brewery':
+    case 'Kernel Brewery':
+    case 'Bodha Drinks':
+    case 'Pastore Brewing & Blending':
+    case 'Floc':
+    case 'Anspach & Hobday':
+    case 'Arbor Ales':
+    case 'Attic Brew Co':
+      return 'North Vault Courtyard'
+
+    case 'Hackney Brewery':
+    case 'Elusive Brewing':
+    case 'Partizan Brewing':
+    case 'Queer Brewing':
+    case 'Abyss Brewing':
+    case 'Exale':
+    case 'Brew York':
+    case 'Hepworth':
+    case 'On Cloud Wine':
+    case 'Go Mate Drinks':
+      return 'North Fountain'
+
+    case 'Turning Point Brew Co':
+    case 'Signature Brew':
+    case 'Lost and Grounded':
+    case 'Big Smoke Brew Co':
+      return 'South Fountain'
+
+    case 'Craft Beerings':
+    case 'Battle of the breweries':
+    case 'Kotinos Foods':
+    case 'Nonna Tonda':
+      return 'Hogshead Passage'
+
+    // Quayside level
+
+    case 'Gosnells of London':
+    case 'Orbit Beers':
+    case 'Magic Rock Brewing':
+    case 'Fourpure':
+    case 'Black lines':
+    case 'Moonwake':
+    case 'The Cocktail Co':
+    case 'Kraken':
+    case 'SXOLLIE':
+    case 'Black Iris':
+    case 'Black Iris Brewery':
+    case 'Alpha Delta':
+    case 'Alpha Delta Brewing':
+    case 'Bianca Road':
+    case 'Luda Brew co':
+    case 'Luda Brewing Co.':
+    case 'Talking Tides':
+    case 'Talking Tides Brew Co':
+    case 'Club Soda (no/lo)':
+      return 'Little Gallery'
+
+    case 'Vintro Bar':
+    case 'CHICK\'IN\'SOURS':
+    case 'Meltdown Burgers':
+      return 'Quayside Walkway'
+
+    case 'Stone & Wood':
+      return 'Quayside, West Mall Bridge'
+
+    case 'Sullivan\'s Brewing Company':
+    case 'Brick Brewery':
+    case 'Howling Hops':
+    case 'Seven Island':
+    case 'Seven Island Brewery':
+    case 'Tempest':
+    case 'Tempest Brewing Co.':
+    case 'Double-Barrelled':
+    case 'JimBrew':
+    case 'Dark Revolution':
+    case 'Dark Revolution ': // yes, with a space
+    case 'Great Beyond':
+    case 'Great Beyond Brewing Company':
+    case 'Virginia Beer Co (USA)':
+    case 'The Virginia Beer Company':
+    case 'Hop the Pond (USA)':
+    case 'Drink Texas - Hop The Pond':
+    case 'Drink Texas - Hop The Pond ':
+    case 'Stone Brewing':
+    case 'Stone Brewing (USA)':
+    case 'Spotlight Bar':
+    case 'Freedom Brewery':
+    case 'The Sexton Whiskey':
+    case 'Dalston\'s Soda Company':
+    case 'Ploom':
+      return 'Quayside, QS1'
+
+    case 'Mondo Brewery':
+    case 'Mondo Brewing Company':
+    case 'Umbrella Cider':
+    case 'Umbrella London':
+    case 'Renegade Brewery':
+    case 'XyloR':
+    case 'Xylo Brewing Ltd':
+    case 'Hammerton':
+    case 'Hammerton Brewery  ': // yes, with two spaces
+      return 'Quayside, QS3'
+
+    case 'Affinity Brewery (LBA BAR)':
+    case 'American Brewers Association (USA)':
+    case 'Brockley Brewery (LBA BAR)':
+    case 'By The Horns (LBA BAR)':
+    case 'Distortion Brewing (LBA BAR)':
+    case 'East London Brewing (LBA BAR)':
+    case 'Friendship Adventure (LBA BAR)':
+    case 'Gravity Well Brewing (LBA BAR)':
+    case 'Great Beyond (LBA BAR)':
+    case 'Hammerton Brewery (LBA BAR)':
+    case 'London Beer Lab (LBA BAR)':
+    case 'London Brewers Alliance':
+    case 'Muswell Hillbilly Brewers (LBA BAR)':
+    case 'ORA (LBA BAR)':
+    case 'Southwark Brewery (LBA BAR)':
+    case 'Tap East (LBA BAR)':
+    case 'Timmermans (BEL)':
+    case 'Timmermans':
+    case 'Wimbledon Brewery (LBA BAR)':
+    case 'Zerodegrees (LBA BAR)':
+      return 'Quayside, QS7'
+
+    case 'Braybrooke Beer Co':
+      return 'Quayside, Food Court'
+
+    case 'Only With Love':
+      return 'Who knows?'
+
+    case 'Vibrant Forest Brewery':
+      return 'Jag vet inte?'
+
+    case 'London Brewing Company':
+      return 'Vet ej tyvärr'
+
+    case 'BRULO':
+      return 'Oklart'
+
+    case 'Neckstamper':
+      return 'Ehhhhh??'
+  }
+
+  return '???'
+}
